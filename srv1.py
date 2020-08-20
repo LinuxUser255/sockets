2b29c67c9da84791a6659eeb812cfeef1b9e4fa1
Chris <atnl@protonmail.com>
2020-08-20 02:23:53 +0000

    Create srv1.py

---
+++ srv1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Bare-bones basic server socket example sending & receiving messages.
+
+import socket
+
+# set up socket object and bind to an IP and port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 4444))
+
+# Now listen for incomming connections
+# 5 consecutive connections qued
+s.listen(8)
+
+# Create While Looop to await connection
+while True:      #clientsocket is a socket object & wemwait for the client's IP  
+    clientsocket, address = s.accept() 
+    # print an f-string for debugging
+    print(f"Connection from {address} successfull? or not?")
+    
+    # Send data to the client socket
+    clientsocket.send(bytes("Sending data in Unicode Transformation Format 8-bit ", "UTF-8"))
