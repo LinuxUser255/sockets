9cf50110a454291132b8835ab93dd859e2c83a74
Chris <atnl@protonmail.com>
2020-08-23 14:49:40 -0400

    Update server.py

--- server.py
+++ server.py
@@ -5,20 +5,20 @@
 import socket
 import threading
 
-
 HEADER = 64 #message byte length 
 PORT = 4444
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!CLOSE CONNECTION!"
+DISCONNECT_MESSAGE = "Connection terminated."
 
 
 # the server's socket object:
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
+#Handle connections & comunications between server & client
+def handle_coms(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
@@ -31,18 +31,18 @@ def handle_client(conn, addr):
         print(f"[{addr}] {msg}")
 
 
-# Begin listening to connections 
-def start():
+# Initiate the socket server & listen for incoming connections
+# The begin thread is allways running
+def begin():
     server.listen()
     # What IP address are we connected to?
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        conn, addr = server.accept()#This line waits on a new connection
+        thread = threading.Thread(target=handle_coms, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
         # the number of active threads = ammount of clients conn
         # because the start thread is allways running then the count begins at -1 (subtract 1)
 
-start()
-
+begin()
